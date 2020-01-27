18670f0fa23c54961344ca7a6ce5792c6079c921
Harsh Wadhwani <harsh.wadhwani@yahoo.co.in>
2020-01-26 18:48:50 -0600

    added addclass button funtionality

---
+++ Grade Calculator/Assets/Scripts/AddClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddClass : MonoBehaviour
+{
+    [SerializeField] private GameObject classButton;
+    [SerializeField] private GameObject canvas;
+
+    public void CreateNewButton()
+    {
+        Instantiate(classButton, new Vector2(0, 0), Quaternion.identity, canvas.transform);
+    }
+}
